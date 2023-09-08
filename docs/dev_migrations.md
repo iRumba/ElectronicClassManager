496a162f7491ea917b97241fdcdf9df6e985e6c1
iRumba <reloader2003@gmail.com>
2023-09-08 14:26:38 +0700

    Добавлены утилита и инструкция для создания миграций

---
+++ docs/dev_migrations.md
@@ -0,0 +1,14 @@
+﻿# Разработческие миграции
+
+## Создание миграций
+Переместиться в папку 
+```src\ElectronicClassManager.Entities.Storage```
+
+открыть cmd или PowerShell
+
+Выполнить команду 
+
+```dotnet ef migrations add ИмяМиграции -s ../ElectronicClassManager.Db.Configuration/ElectronicClassManager.Db.Configuration.csproj```
+
+Или перейти в папку utils и выполнить команду 
+```cm.bat ИмяМиграции```
---
+++ utils/cm.bat
@@ -0,0 +1,9 @@
+﻿echo off
+SET wd=..\src\ElectronicClassManager.Entities.Storage
+SET su=..\ElectronicClassManager.Db.Configuration\ElectronicClassManager.Db.Configuration.csproj
+SET p=..\src\ElectronicClassManager.Entities.Storage\ElectronicClassManager.Entities.Storage.csproj
+SET sufull=..\src\ElectronicClassManager.Db.Configuration\ElectronicClassManager.Db.Configuration.csproj
+
+rem cd %wd%
+
+dotnet ef migrations add %1 -s %sufull% -p %p%
